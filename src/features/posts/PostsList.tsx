ebe52c26994785f7a963253049c0810fcaaf0538
moretime-dev <moretimeproductions@googlemail.com>
2022-05-03 17:28:56 +0200

    adding date and reactions

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -2,16 +2,22 @@ import { useAppSelector } from "../../app/hooks";
 import { selectAllPosts } from "./postsSlice";
 
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 
 const PostsList: React.FC = () => {
   const posts = useAppSelector(selectAllPosts);
 
-  const renderedPosts = posts.map((post) => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
       <p className="postCredit">
         <PostAuthor userId={post.userId ? post.userId : ""} />
+        <TimeAgo timestamp={post.date} />
       </p>
     </article>
   ));
--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,11 +1,21 @@
 import { createSlice, PayloadAction, nanoid } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 
+import { sub } from "date-fns";
+
 export interface Post {
   id: string;
   title: string;
   content: string;
   userId?: string;
+  date: string;
+  reactions: {
+    thumbsUp: number;
+    wow: number;
+    heart: number;
+    rocket: number;
+    coffee: number;
+  };
 }
 
 const initialState: Post[] = [
@@ -13,11 +23,27 @@ const initialState: Post[] = [
     id: "1",
     title: "Learning Redux Toolkit",
     content: "I've heard good things",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
   {
     id: "2",
     title: "Slices...",
     content: "The more I say slice, the more I want pizza...",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
 ];
 
@@ -35,7 +61,15 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
+            date: new Date().toISOString(),
             userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
           },
         };
       },
